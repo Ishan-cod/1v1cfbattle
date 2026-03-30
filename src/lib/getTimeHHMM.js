7a57f2f847bc176e3123ed0e5c64124a425b418f
Ishan Jaiswal <ishanj626@gmail.com>
2026-03-30 17:12:30 +0530

    base implementation of chat section

---
+++ src/lib/getTimeHHMM.js
@@ -0,0 +1,9 @@
+const getTimeHHMM = () => {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+};
+
+export { getTimeHHMM };
