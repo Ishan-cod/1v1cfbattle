0c7407b12ec4aac016934772d729445671f1bf4b
Ishan Jaiswal <ishanj626@gmail.com>
2026-04-09 10:57:14 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
         {children}
         <Analytics />
       </body>
+      <GoogleAnalytics gaId="G-EXHQH177J5"/>
     </html>
   );
 }
